b63753f72bbf4d339cc18be42f8185297a473231
Ethan <ethantpainter@gmail.com>
2024-12-08 22:03:18 -0500

    add detection and transition to game screen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,18 +2,37 @@
   import '../static/fonts.css';
   import { createProvidedUsers } from "../store/players.svelte";
   import SelectUsers from "$lib/select-users.svelte";
+	import { slide } from 'svelte/transition';
+	import PlinkoGame from '$lib/plinko-game.svelte';
 
   let users = createProvidedUsers([]);
   const areUsersProvided = $state(false);
-  const setStage = $state("ENTER_USERS");
+  let setStage = $state("ENTER_USERS");
+
+  function updateStage(newStage: string) {
+    setStage = newStage;
+  }
 </script>
 
-<h1 class="header">Welcome!</h1>
-<h2 class="header">Enter the names of everyone who wants to play Plinko!</h2>
-
+<!-- Enter user information -->
 {#if setStage === "ENTER_USERS"}
-  <button class="play-button">Play</button>
-  <SelectUsers setUsers={users.setUsers} currentUsers={users.users}/>
+  <div transition:slide={{ duration: 500 }}>
+    <h1 class="header">Welcome!</h1>
+    <h2 class="header">Enter the names of everyone who wants to play Plinko!</h2>
+  
+    {#if users.users.length > 0}
+      <button class="play-button" onclick="{() => updateStage("PLAY_GAME")}">Start Game!</button>
+    {/if}
+  
+    <SelectUsers setUsers={users.setUsers} currentUsers={users.users}/>
+  </div>
+{/if}
+
+<!-- Play the Game! -->
+{#if setStage === "PLAY_GAME"}
+  <div transition:slide={{ delay: 500, duration: 400 }}>
+    <PlinkoGame />
+  </div>
 {/if}
 
 <style>
@@ -27,5 +46,8 @@
   .play-button {
     display: block;
     margin: 6px auto;
+    padding: 6px;
+    font-size: 1rem;
+    font-weight: 500;
   }
 </style>
